56fab45f8926c9260dddb215086c5fd156d8db89
devjanaprime <dev@primeacademy.io>
2016-10-12 10:06:57 -0500

    added redirects

--- server/upload.php
+++ server/upload.php
@@ -29,7 +29,7 @@ if( $uploadOk == 0 ) {
 // if everything is ok, try to upload file
 } else {
     if( move_uploaded_file( $_FILES[ "fileToUpload" ][ "tmp_name" ], $target_file ) ) {
-        echo 'file uploaded: uploads/' . basename( $_FILES["fileToUpload"]["name"]);
+        header( 'Location: ../client/create.html' ) ;
     } else {
         echo "Yikes, there was an error uploading your file. ";
     }
